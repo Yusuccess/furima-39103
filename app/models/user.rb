5957592a890caa41653c506382bcb4b33fd61c3f
Yusuccess <world2017norway@gmail.com>
2023-08-06 15:20:31 +0900

    ユーザーモデルのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+    validates :last_name_reading, presence: true
+    validates :first_name_reading, presence: true
+    validates :nickname, presence: true
+    validates :birthday, presence: true
+    validates :email, presence: true
+    validates :password, presence:true
+  end
+
 end
