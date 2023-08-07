e774b5db973db87cca67df2a06656ef2704987cb
Yusuccess <world2017norway@gmail.com>
2023-08-07 15:33:32 +0900

    エラーの表示
    エラー分に重複あったため、バリデーションのpasswordを消去

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,11 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
-    validates :last_name_reading, presence: true
-    validates :first_name_reading, presence: true
+    validates :last_name_reading, presence: true, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :first_name_reading, presence: true, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :nickname, presence: true
     validates :birthday, presence: true
     validates :email, presence: true
-    validates :password, presence:true
+    #validates :password, presence:true
   end
-
 end
