d60899a38ebd8e48ca1670291c8f5cfca333fe65
Yusuccess <world2017norway@gmail.com>
2023-08-08 22:27:57 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,23 @@
 class Item < ApplicationRecord
+  validates :image,                 presence: true
+  validates :name,                  presence: true
+  validates :details,               presence: true
+  validates :category_id,           presence: true
+  validates :condition_id,          presence: true
+  validates :shipping_fee_id,       presence: true
+  validates :shipping_day_id,       presence: true
+  validates :prefecture_id,         presence: true
 
+  
+  belongs_to :user
+  has_one :purchase
+  has_one_attached :image
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
   belongs_to :prefecture
   belongs_to :shipping_day
   belongs_to :shipping_fee
-  
+
 end
