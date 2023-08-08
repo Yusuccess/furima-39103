b8d2cca1b987e89f383050c764720755c832d68c
Yusuccess <world2017norway@gmail.com>
2023-08-08 22:32:18 +0900

    商品出品ページへの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,16 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
   end
-  
+  def new
+  end
+
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
