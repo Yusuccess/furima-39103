531f03de28b9696abffd4cbca0efa40ac20d7c87
Yusuccess <world2017norway@gmail.com>
2023-08-08 17:31:12 +0900

    商品テーブルの作成_2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_30_075819) do
+ActiveRecord::Schema.define(version: 2023_08_08_080430) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.text "details", null: false
+    t.integer "shipping_fee_id", null: false
+    t.integer "shipping_day_id", null: false
+    t.bigint "user_id", null: false
+    t.integer "prefecture_id", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "last_name", null: false
@@ -30,4 +43,5 @@ ActiveRecord::Schema.define(version: 2023_07_30_075819) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
