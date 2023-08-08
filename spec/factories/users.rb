5738b049c3724cee4d3df05e20eea4b715b38dcd
Yusuccess <world2017norway@gmail.com>
2023-08-08 13:18:10 +0900

    テストコードの修正
    ・新規登録できる時を追記
    ・新規登録できない時のテスト項目の追記

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,16 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
+    nickname              {Faker::Name.initials(number: 6)}
+    email                 {Faker::Internet.free_email}
     last_name             {'田中'}
     first_name            {'佑介'}
     last_name_reading     {'タナカ'}
     first_name_reading    {'ユウスケ'}
     birthday              {'1993-09-03'}
-    password              {'000000'}
+    password {Faker::Internet.password(min_length: 6)}
+    #password              {Faker::Lorem.characters(min_length:6, max_length: 128, min_alpha: 1, min_numeric: 1)}
     password_confirmation {password}
+
+
   end
 end
