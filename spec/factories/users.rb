a0c89fb7670286b5e1d1c78243aaf5398c052cbb
Yusuccess <world2017norway@gmail.com>
2023-08-08 15:55:30 +0900

    テストコードの修正_4

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,18 @@
 FactoryBot.define do
   factory :user do
+    transient do
+      person { Gimei.name }
+    end
     nickname              {Faker::Name.name}
     email                 {Faker::Internet.free_email}
-    last_name             {'田中'}
-    first_name            {'佑介'}
-    last_name_reading     {'タナカ'}
-    first_name_reading    {'ユウスケ'}
+    last_name             {person.last.kanji}
+    first_name            {person.first.kanji}
+    last_name_reading     {person.last.katakana}
+    first_name_reading    {person.first.katakana}
     birthday              {Faker::Date.birthday(min_age: 18, max_age: 65)}
     password              {Faker::Lorem.characters(number: 10, min_alpha: 1, min_numeric: 1)}
     password_confirmation {password}
 
-
+    
   end
 end
