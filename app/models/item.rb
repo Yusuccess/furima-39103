73cdc79d8b052730b2436a7d27ee54145d1b22b4
Yusuccess <world2017norway@gmail.com>
2023-08-10 21:07:07 +0900

    バリデーションの設定_3

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,19 +1,22 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
   validates :image,                 presence: true
   validates :name,                  presence: true
   validates :details,               presence: true
-  validates :category_id,           numericality: { other_than: 1 , message: "can't be blank"}
-  validates :condition_id,          numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :prefecture_id,         numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_day_id,       numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_fee_id,       numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price,                 presence: true,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },format: { with: /\A[0-9]+\z/ }
+  validates :user_id,               presence: true
+  validates :category_id,           presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id,          presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id,         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id,       presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_fee_id,       presence: true, numericality: { other_than: 1 , message: "can't be blank"}
 
   belongs_to :user
   has_one :purchase
   has_one_attached :image
   
-  extend ActiveHash::Associations::ActiveRecordExtensions
+  
   belongs_to :category
   belongs_to :condition
   belongs_to :prefecture
