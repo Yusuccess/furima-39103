2aac4d6e8af144d427670bdb0471feb56009b60e
Yusuccess <world2017norway@gmail.com>
2023-08-10 21:06:44 +0900

    Createアクションの定義
    user_idの情報もマージした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,13 +4,23 @@ class ItemsController < ApplicationController
   def index
   end
   def new
+    @item = Item.new
   end
+  def create
+    #binding.pry
+    @item = Item.new(item_params)
 
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
 
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :details, :price, :category_id, :condition_id, :prefecture_id, :shipping_fee_id, :shipping_day_id).merge(user_id: current_user.id)
   end
 end
