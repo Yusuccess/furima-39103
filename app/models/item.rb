2b7fabbaee624e19eb2d164072de4dd5a62f7301
Yusuccess <world2017norway@gmail.com>
2023-08-11 21:51:27 +0900

    Integer型整数値のバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ApplicationRecord
   validates :image,                 presence: true
   validates :name,                  presence: true
   validates :details,               presence: true
-  validates :price,                 presence: true,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price,                 presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :category_id,           presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id,          presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :prefecture_id,         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
