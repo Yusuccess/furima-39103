8fcb3629e7b418d2e493f842011bd5a0e26d9532
Yusuccess <world2017norway@gmail.com>
2023-08-11 21:23:17 +0900

    デバック用コードの削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,9 +8,8 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
   def create
-    #binding.pry
     @item = Item.new(item_params)
-
+    
     if @item.save
       redirect_to root_path
     else
