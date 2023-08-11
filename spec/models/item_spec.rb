510398e76e64f6adcc78e0345c2140cbf2bef682
Yusuccess <world2017norway@gmail.com>
2023-08-11 21:59:06 +0900

    テストコードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before do
-    @item = FactoryBot.create(:item)
+    @item = FactoryBot.build(:item)
   end
 
   describe 'ユーザー新規登録' do
