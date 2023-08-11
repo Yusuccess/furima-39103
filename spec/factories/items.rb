a49986301e38b16ba8eb2821deba6d254711f62c
Yusuccess <world2017norway@gmail.com>
2023-08-11 19:43:49 +0900

    出品機能のテストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
+    name                 {Faker::Name.name}
+    details              {Faker::Lorem.sentence}
+    category_id          {Faker::Number.between(from: 2, to: 11)}
+    condition_id         {Faker::Number.between(from: 2, to: 7)}
+    shipping_fee_id      {Faker::Number.between(from: 2, to: 3)}
+    shipping_day_id      {Faker::Number.between(from: 2, to: 4)}
+    prefecture_id        {Faker::Number.between(from: 2, to: 48)}
+    price                {9999}
+
+    association :user 
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
+  
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -12,7 +12,5 @@ FactoryBot.define do
     birthday              {Faker::Date.birthday(min_age: 18, max_age: 65)}
     password              {Faker::Lorem.characters(number: 10, min_alpha: 1, min_numeric: 1)}
     password_confirmation {password}
-
-    
   end
 end
