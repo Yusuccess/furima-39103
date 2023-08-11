f3ba16700fdeb6533d1a5add410665113446405e
Yusuccess <world2017norway@gmail.com>
2023-08-11 21:29:52 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,6 @@ class Item < ApplicationRecord
   validates :name,                  presence: true
   validates :details,               presence: true
   validates :price,                 presence: true,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  validates :user_id,               presence: true
   validates :category_id,           presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id,          presence: true, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :prefecture_id,         presence: true, numericality: { other_than: 1 , message: "can't be blank"}
