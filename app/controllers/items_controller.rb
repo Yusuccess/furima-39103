8dd048d4fe5ace2c1fa0e4eb36b3ef2cbdffb9be
Yusuccess <world2017norway@gmail.com>
2023-08-13 17:51:51 +0900

    商品詳細ページへの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,9 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+  def show
+    @item = Item.find(params[:id])
+  end
 
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items ,only: [:index, :new, :create]
+  resources :items ,only: [:index, :new, :create, :show] do
+    #resources :items, only: [:get]
+  end
+  
   resources :users
 end
