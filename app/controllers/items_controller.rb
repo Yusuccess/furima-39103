13f887d454076484c3ef5de7cb304417f4c51a97
Yusuccess <world2017norway@gmail.com>
2023-08-14 15:46:47 +0900

    updateアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,9 @@
 class ItemsController < ApplicationController
   
   before_action :authenticate_user!, only: [:new]
-  
+  before_action :set_item, except: [:index, :new, :create]
+  before_action :contributor_confirmation, only: [:edit, :update]
+
   def index
     @items = Item.order("created_at DESC")
     @purchase = Purchase.all
@@ -22,13 +24,30 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
   def edit
-
+    @item = Item.find(params[:id])
+    #binding.pry
   end
 
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :details, :price, :category_id, :condition_id, :prefecture_id, :shipping_fee_id, :shipping_day_id).merge(user_id: current_user.id)
   end
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @item.user
+  end
+
+
 end
