5013c2cd7ecda055cc36d7155fbb178d386dc923
Yusuccess <world2017norway@gmail.com>
2023-08-16 17:42:34 +0900

    Javaを使用した購入画面のid修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -6,6 +6,8 @@ window.addEventListener('load', () => {
     addTaxDom.innerHTML = Math.floor(inputValue*0.1)
 
     const Total = document.getElementById("profit");
-    Total.innerHTML = Math.floor(inputValue*1.1)
+    Total.innerHTML = Math.floor(inputValue*0.9)
   })
 });
+
+
