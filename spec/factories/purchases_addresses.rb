08a1ef93da47079882cbb8f7c30c645c3f884e8d
Yusuccess <world2017norway@gmail.com>
2023-08-16 20:34:40 +0900

    FactoryBotの修正

--- spec/factories/purchases_addresses.rb
+++ spec/factories/purchases_addresses.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     prefecture_id        {Faker::Number.between(from: 2, to: 48)}
     city                 {"名古屋市"}
     house_number         {"武田2-190"}
+    building_name        {"レジデンス田中509"}
     phone_number         {"09012345678"}
     token                {"tok_abcdefghijk00000000000000000"}
   end
